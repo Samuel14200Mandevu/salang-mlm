2b0214a973da1797378c5068e7d3304b95ac22db
Samuel MANDEVU <samuelmandevu10@gmail.com>
2026-06-30 19:59:39 +0200

    Préparation pour déploiement Vercel

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Charger l'autoloader de Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Charger l'application Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Exécuter la requête
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+// Envoyer la réponse
+$response->send();
+$kernel->terminate($request, $response);
